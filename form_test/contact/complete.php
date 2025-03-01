a25f376c9a8ade64de490a3b32b4922a7939267f
KensakuShimomura <149616984+KensakuShimomura@users.noreply.github.com>
2025-03-01 17:09:54 +0900

    Add files via upload

--- form_test/contact/complete.php
+++ form_test/contact/complete.php
@@ -1,9 +1,9 @@
 <?php
 $mailto = $_POST[ "email" ];
 $subject = "お問い合わせ";
-$header = "From:123@abc.com";
+$header = "From:info@abc.com";
 $header .= "\n";
-$header .= "Bcc:345@abc.com";
+$header .= "Bcc:info@abc.com";
 $message = "***************************************************" . "\n" . "\n" .
 $_POST[ "name" ] . " 様、お問い合わせを頂き、誠にありがとうございます。" . "\n" .
 "追って担当者よりご連絡させていただきます。" . "\n" . "\n" .
@@ -17,41 +17,60 @@ $_POST[ "name" ] . " 様、お問い合わせを頂き、誠にありがとう�
 "ご職業：" . $_POST[ "profession" ] . "\n" .
 "件名：" . $_POST[ "subject" ] . "\n" .
 "住所：〒" . $_POST[ "zip" ] . "\n" . $_POST[ "pref" ] . $_POST[ "address" ] . $_POST[ "other_address" ] . "\n" .
-"電話番号：" . $_POST[ "phonenumber" ] . "\n" .
+"電話番号：" . $_POST[ "tel" ] . "\n" .
 "法人様向け：" . $_POST[ "company_menu" ] . "\n" .
-"お問い合わせ種別：" . $_POST[ "contact_types" ] . "\n" .
+"お問い合わせ種別：" . $_POST[ "contact_type" ] . "\n" .
+"ご連絡方法：" . $_POST[ "contact_means" ] . "\n" .
 "お問い合せ内容：" . "\n" . $_POST[ "comment" ];
 
 mb_internal_encoding( "UTF-8" );
 
-// メール送信
-$email = $_POST["email"];
-if (!empty($email)) {
-mb_send_mail( $mailto, $subject, $message, $header );
+session_start();
+// POSTされたトークンを取得
+$token = isset($_POST["token"]) ? $_POST["token"] : "";
+// セッション変数のトークンを取得
+$session_token = isset($_SESSION["token"]) ? $_SESSION["token"] : "";
+// セッション変数のトークンを削除
+unset($_SESSION["token"]);
+// POSTされたトークンとセッション変数のトークンの比較をして一致したら
+if ($token != "" && $token == $session_token) {
+
+  //メール送信
+  mb_send_mail( $mailto, $subject, $message, $header );
+  
+  //文言
+  $thanks = "お問い合わせありがとうございました。";
+  $thanks2 = "ご入力内容を送信しました。<br>3日たっても返信がない場合はパソコンやスマートフォンの設定をご確認の上、恐れ入りますが再度ご連絡を頂戴できればと存じます。";
+    
+} else {
+    
+   $thanks = "セッションエラーです。";
+   $thanks2 = "";
 }
-else {}
 
 ?>
-<!doctype html>
-<html>
+
+<!DOCTYPE html>
+<html lang="ja">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="お問い合わせフォームテスト" />
-<title> お問い合わせ | ご入力内容送信 </title>
-<link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせ</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
 <header>
-  <div class="wrap">
+  <div class="wrapper">
       <h1>お問い合わせフォーム</h1>
   </div>
 </header>
-  <div class="wrap">
-    <h1 class="mb_20 ta_center text_20"> お問い合わせありがとうございます。</h1>
-    <p class="ta_center">ご入力内容を送信しました。<br>
-      3日たっても返信がない場合はパソコンやスマートフォンの設定をご確認の上、恐れ入りますが再度ご連絡を頂戴できればと存じます。 </p>
-  </div>
+        <div class="wrapper">
+        <h2 class="mb_20 ta_center text_20"><?php echo $thanks ?></h2>
+        <p class="ta_center"><?php echo $thanks2 ?></p>
+        </div>
 </body>
+
 </html>
--- form_test/contact/confirm.php
+++ form_test/contact/confirm.php
@@ -1,9 +1,19 @@
 <?php
 
+session_start();
+// 二重送信防止用トークンの発行
+$token = uniqid('', true);
+//トークンをセッション変数にセット
+$_SESSION['token'] = $token;
+
 function convert_char( $target ) {
   $target = stripslashes( $target );
   $target = htmlspecialchars( $target, ENT_QUOTES );
   return $target;
+}
+
+if (isset($_POST['contact_type']) && is_array($_POST['contact_type'])) {
+    $contact_type = implode("、", $_POST["contact_type"]);
 }
 
 $name = convert_char( $_POST[ "name" ] );
@@ -15,108 +25,110 @@ $zip = convert_char( $_POST[ "zip" ] );
 $pref = convert_char( $_POST[ "pref" ] );
 $address = convert_char( $_POST[ "address" ] );
 $other_address = convert_char( $_POST[ "other_address" ] );
-$phonenumber = convert_char( $_POST[ "phonenumber" ] );
+$tel = convert_char( $_POST[ "tel" ] );
 $company_menu = convert_char( $_POST[ "company_menu" ] );
+$contact_type = convert_char( $contact_type );
+$contact_means = convert_char( $_POST[ "contact_means" ] );
 $comment = convert_char( $_POST[ "comment" ] );
 
-if (isset($_POST['contact_type']) && is_array($_POST['contact_type'])) {
-  $contact_types = implode("、", $_POST["contact_type"]);
-}
-$contact_types = convert_char( $contact_types );
+$comment_conv = nl2br( $comment, false );
 
-$comment = str_replace( "\r\n", "\n", $comment );
-$comment = str_replace( "\r", "\n", $comment );
-$comment_conv = str_replace( "\n", "<br>", $comment );
 ?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="ja">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="keywords" content="お問い合わせフォームテスト" />
-<title> お問い合わせ | 入力内容確認</title>
-<link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせ</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
 <header>
-  <div class="wrap">
+  <div class="wrapper">
       <h1>お問い合わせフォーム</h1>
   </div>
 </header>
-  <div class="wrap">
-    <h1 class="heading_a mb_30">ご入力内容確認</h1>
-  </div>
-  <div class="wrap">
-    <form action="complete.php" method="post">
+<div class="wrapper">
+    <form action="complete.php" method="post" id="contact_form" class="h-adr">
       <div class="sp100 of_x">
-        <table id="contactform_table" class="width81">
+        <table id="contactform_table" class="width81 mb_30">
           <tr>
             <th>お名前</th>
-            <td><?php print $name; ?></td>
+            <td><?php echo $name; ?></td>
           </tr>
           <tr>
             <th>ふりがな</th>
-            <td><?php print $huri; ?></td>
+            <td><?php echo $huri; ?></td>
           </tr>
           <tr>
             <th>メールアドレス</th>
-            <td><?php print $email; ?></td>
+            <td><?php echo $email; ?></td>
           </tr>
           <tr>
             <th>ご職業</th>
-            <td><?php print $profession; ?></td>
+            <td><?php echo $profession; ?></td>
           </tr>
           <tr>
             <th>件名</th>
-            <td><?php print $subject; ?></td>
+            <td><?php echo $subject; ?></td>
           </tr>
           <tr>
             <th>住所</th>
-            <td><?php print $zip; ?><br>
+            <td>
+              〒<?php echo $zip; ?>
               <br>
-              <?php print $pref; ?> <br>
-              <?php print $address; ?><br>
+              <?php echo $pref; ?>
               <br>
-              <?php print $other_address; ?><br></td>
+              <?php echo $address; ?>
+              <br>
+              <?php echo $other_address; ?></td>
           </tr>
           <tr>
             <th>電話番号</th>
-            <td><?php print $phonenumber; ?></td>
+            <td><?php echo $tel; ?></td>
           </tr>
           <tr>
             <th>法人様向け</th>
-            <td><?php print $company_menu; ?></td>
+            <td><?php echo $company_menu; ?></td>
           </tr>
           <tr>
             <th>お問い合わせ種別</th>
-            <td><?php print $contact_types; ?></td>
+            <td><?php echo $contact_type; ?></td>
           </tr>
           <tr>
-            <th>お問い合わせ内容</th>
-            <td><?php print $comment_conv; ?></td>
+            <th>ご連絡方法</th>
+            <td><?php echo $contact_means; ?></td>
+          </tr>
+          <tr>
+            <th>お問い合わせ内容</label></th>
+            <td><?php echo $comment_conv; ?></td>
           </tr>
           <tr>
             <th></th>
-            <td><input type="button" name="return" value="戻る" id="return" onClick="history.back()" class="form_btn mr_20">
-              <input type="submit" name="Submit" value="送信" id="submit" class="form_btn mr_20"></td>
+            <td><input type="button" value="戻る" onclick="history.back()"> <input type="submit" name="Submit" value="送信" id="submit" class="form_btn"></td>
           </tr>
         </table>
       </div>
-      <input type="hidden" name="name" value="<?php echo $_POST['name']; ?>" />
-      <input type="hidden" name="huri" value="<?php echo $_POST['huri']; ?>" />
-      <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>" />
-      <input type="hidden" name="profession" value="<?php echo $_POST['profession']; ?>" />
-      <input type="hidden" name="subject" value="<?php echo $_POST['subject']; ?>" />
-      <input type="hidden" name="zip" value="<?php echo $_POST['zip']; ?>" />
-      <input type="hidden" name="pref" value="<?php echo $_POST['pref']; ?>" />
-      <input type="hidden" name="address" value="<?php echo $_POST['address']; ?>" />
-      <input type="hidden" name="other_address" value="<?php echo $_POST['other_address']; ?>" />
-      <input type="hidden" name="phonenumber" value="<?php echo $_POST['phonenumber']; ?>" />
-      <input type="hidden" name="company_menu" value="<?php echo $_POST['company_menu']; ?>" />
-      <input type="hidden" name="contact_types" value="<?php echo $contact_types; ?>" />
-      <input type="hidden" name="comment" value="<?php echo $_POST['comment']; ?>" />
+        <input type="hidden" name="name" value="<?php echo $_POST['name']; ?>">
+        <input type="hidden" name="furi" value="<?php echo $_POST['furi']; ?>">
+        <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+        <input type="hidden" name="profession" value="<?php echo $_POST['profession']; ?>">
+        <input type="hidden" name="subject" value="<?php echo $_POST['subject']; ?>">
+        <input type="hidden" name="zip" value="<?php echo $_POST['zip']; ?>">
+        <input type="hidden" name="pref" value="<?php echo $_POST['pref']; ?>">
+        <input type="hidden" name="address" value="<?php echo $_POST['address']; ?>">
+        <input type="hidden" name="other_address" value="<?php echo $_POST['other_address']; ?>">
+        <input type="hidden" name="tel" value="<?php echo $_POST['tel']; ?>">
+        <input type="hidden" name="tel" value="<?php echo $_POST['company_menu']; ?>">
+        <input type="hidden" name="contact_type" value="<?php echo $contact_type; ?>">
+        <input type="hidden" name="comment" value="<?php echo $_POST['comment']; ?>">
+        <input type="hidden" name="token" value="<?php echo $token; ?>">
     </form>
   </div>
+
 </body>
+
 </html>
